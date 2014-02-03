83059949f46303e2fa4c6149126dab6fe34d74a0
Özgür Sarıer <ozgursarier1011601115@gmail.com>
2014-02-04 01:11:27 +0200

    Neuro 2.1.01

--- functions.php
+++ functions.php
@@ -403,10 +403,6 @@ remove_action ('load-update-core.php', 'wp_update_themes');
 add_filter ('pre_site_transient_update_themes',create_function ('$a', "return null;"));
 /** -----------------------END-------------------- */
 
-/**
-* End
-*/
-
 
 /**
 * End
